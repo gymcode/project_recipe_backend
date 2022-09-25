340101ee205438334bb0f0a118646298253cbbe0
gymcode <kyleabs20@gmail.com>
2022-09-25 23:07:54 +0000

    struct for recipe model

--- model/recipe.go
+++ model/recipe.go
@@ -1,7 +1,11 @@
 package model
 
 type Recipe struct {
-	name        string
-	healthScore int
-	
+	name            string
+	healthScore     int
+	aggragatedLikes int
+	servings        int
+	glutenFree      bool
+	pricePerServing float64
+	readyInMinutes  int
 }
