f6bbb046043de188e4947f9f3f2686e554cceda8
gymcode <kyleabs20@gmail.com>
2022-09-27 23:34:08 +0000

    the recipe controller logic

--- controller/recipeController.go
+++ controller/recipeController.go
@@ -1,8 +1,52 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"database/sql"
+	"log"
 
-func AddRecipe(c *fiber.Ctx) error{
+	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gymcode/project_recipe_backend/database"
+	"github.com/gymcode/project_recipe_backend/model"
+	utils "github.com/gymcode/project_recipe_backend/utils"
+)
+
+func AddRecipe(c *fiber.Ctx) error {
+	// get the cookies
+	cookie := c.Cookies("token")
+
+	log.Println(cookie)
+	// parsing with claims
+	token, _ := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return utils.Secret, nil
+	})
+
+	if token == nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(
+			model.WrapFailureResponse{
+				Code:    "01",
+				Message: "Unauthorized :: you do not have access to get details",
+				Error:   true,
+			})
+	}
+
+	// get the user
+
+	// convert it to the standard claim
+	claim := token.Claims.(*jwt.StandardClaims)
+
+	var user model.User
+
+	database.DB.Where("id = @id", sql.Named("id", claim.Issuer)).Find(&user)
+
+	if user.ID == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(model.WrapFailureResponse{
+			Code:    "01",
+			Message: "User with email does not exist",
+			Error:   false,
+		})
+	}
+
 	return c.JSON(fiber.Map{
 		"message": "output",
 	})
