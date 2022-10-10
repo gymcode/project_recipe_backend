2fce6bb8c3e61383c96667f00461dfc3fcfd4af8
gymcode <kyleabs20@gmail.com>
2022-10-10 22:46:29 +0000

    errors

---
+++ model/models.go
@@ -0,0 +1,18 @@
+package model
+
+type SmsObject struct {
+	Messages    []MessagesObject `json: "messages"`
+	Message_Glb GlobalMessages   `json: "message_globals"`
+}
+
+type MessagesObject struct {
+	Channel string `json: "channel"`
+	Recipients []string 	`json: "recipients"`
+	Content string `json: "content"`
+	Msg_Type string `json: "msg_type"`
+	Data_Coding string 	`json:"data_coding"`
+}
+
+type GlobalMessages struct {
+	Originator string `json: "originator"`
+}
--- utils/smsPackage.go
+++ utils/smsPackage.go
@@ -1,13 +1,63 @@
 package utils
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"strings"
 
 	"github.com/twilio/twilio-go"
 	twilioAPI "github.com/twilio/twilio-go/rest/api/v2010"
 )
 
-func SendSms(message string, recipient string) bool{
+func SendSms() {
+	url := "https://api.d7networks.com/messages/v1/send"
+	method := "POST"
+
+	payload := strings.NewReader(`{
+	"messages": [
+		{
+			"channel": "sms",
+			"recipients": ["+233268211334","+233268211334"],
+			"content": "Greetings from D7 API",
+			"msg_type": "text",
+			"data_coding": "text"
+		}
+	],
+	"message_globals": {
+		"originator": "Haute Cuisine OTP",
+		"report_url": "https://the_url_to_recieve_delivery_report.com"
+	}
+	}`)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+	
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
+}
+
+func SendSmsDummy(message string, recipient string) bool {
 	accountSid := ""
 	authToken := ""
 
@@ -26,7 +76,7 @@ func SendSms(message string, recipient string) bool{
 	log.Println(err)
 
 	if err != nil {
-		return false 
+		return false
 	} else {
 		return true
 	}
